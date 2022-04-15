4d1749c61060aba555571be83318b9a98268d3b6
Vahablotfi <vahablotfi1@gmail.com>
2022-04-15 21:06:22 +0200

    formatted

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,146 +1,105 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("../database");
-const  inputValidation  = require("../validations/mealsValidation")
-const validationFilter=require("../validations/filterValidation")
-
-
+const inputValidation = require("../validations/mealsValidation");
+const validationFilter = require("../validations/filterValidation");
 const currentDate = new Date();
 
 router.get("/", validationFilter, async (request, response) => {
-    console.log('this is date',currentDate);
-     let meals = await knex("meals");
-    try {
-       
-        if ('id' in request.query) {
-               meals = await knex('meals')
-                .select()
-                .where('id', '=',request.query.id);
-        }
-        if ('maxPrice' in request.query) {
-            meals = await knex('meals')
-                .select()
-                .where('price', '<=', request.query.maxPrice);
-        }
-        if ('createdAfter' in request.query) {
-            meals = await knex('meals')
-                .select()
-                .where('created_date', '>', request.query.createdAfter);
-        }
-        if ('availableReservations' in request.query) {
-            const totalReservation = await knex('reservations')
-                .select("meal_id", "title", "max_number_of_guests")
-                .sum('number_of_guests AS reserved_guests')
-                .join('meals', "meal_id", "=", "meals.id")
-                .groupBy('meal_id');
-
-            meals = totalReservation.filter(reservation => 
-                Number(reservation.reserved_guests) < reservation.max_number_of_guests);
-        }
-        if ('title' in request.query) {
-            meals = await knex('meals')
-                .select()
-                .where('title', 'like', `%${(request.query.title).toLowerCase()}%`);
-        }
-        if ('limit' in request.query) {
-             meals = await knex('meals')
-                .select()
-                .limit(request.query.limit);
-        }
-        if('maxPrice' in request.query &&
-            'limit' in request.query) {
-            meals = await knex('meals').select()
-                    .where('price', '<=', request.query.maxPrice)
-                    .limit(request.query.limit)
-                    .orderBy('price');
-                   }
-        response.send(meals);
-        
-    } catch (error) {
-        throw error;
+  console.log("this is date", currentDate);
+  let meals = await knex("meals");
+  try {
+    if ("id" in request.query) {
+      meals = await knex("meals").select().where("id", "=", request.query.id);
     }
-})
-
-
-
-
-
-
-
-
-
-router.post("/",inputValidation, async(request, response) => {
-    try {
-        // knex syntax for selecting things. Look up the documentation for knex for further info
-        const insertRequest = request.body;
-        const meals = await knex("meals").insert({
-             
-            id: insertRequest.id,
-            title: insertRequest.title,
-            description: insertRequest.description,
-            location: insertRequest.location,
-            Hosting_time: insertRequest.Hosting_time,
-            max_number_of_guests: insertRequest.max_number_of_guests,
-            price: insertRequest.price,
-            created_date: currentDate,
-        });
-        response.json(meals);
-    } catch (error) {
-        throw error;
+    if ("maxPrice" in request.query) {
+      meals = await knex("meals")
+        .select()
+        .where("price", "<=", request.query.maxPrice);
     }
+    if ("createdAfter" in request.query) {
+      meals = await knex("meals")
+        .select()
+        .where("created_date", ">", request.query.createdAfter);
+    }
+    if ("availableReservations" in request.query) {
+      const totalReservation = await knex("reservations")
+        .select("meal_id", "title", "max_number_of_guests")
+        .sum("number_of_guests AS reserved_guests")
+        .join("meals", "meal_id", "=", "meals.id")
+        .groupBy("meal_id");
+
+      meals = totalReservation.filter(
+        (reservation) =>
+          Number(reservation.reserved_guests) < reservation.max_number_of_guests
+      );
+    }
+    if ("title" in request.query) {
+      meals = await knex("meals")
+        .select()
+        .where("title", "like", `%${request.query.title.toLowerCase()}%`);
+    }
+    if ("limit" in request.query) {
+      meals = await knex("meals").select().limit(request.query.limit);
+    }
+    if ("maxPrice" in request.query && "limit" in request.query) {
+      meals = await knex("meals")
+        .select()
+        .where("price", "<=", request.query.maxPrice)
+        .limit(request.query.limit)
+        .orderBy("price");
+    }
+    response.send(meals);
+  } catch (error) {
+    throw error;
+  }
 });
-                
 
-router.put("/",validationFilter, async (request,response ) => {
-    try {
-      
-      const result = await knex("meals")
-          .where("id", "=", request.query.id)
-          .update(request.body);
-       if (result > 0) {
+router.post("/", inputValidation, async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const insertRequest = request.body;
+    const meals = await knex("meals").insert({
+      id: insertRequest.id,
+      title: insertRequest.title,
+      description: insertRequest.description,
+      location: insertRequest.location,
+      Hosting_time: insertRequest.Hosting_time,
+      max_number_of_guests: insertRequest.max_number_of_guests,
+      price: insertRequest.price,
+      created_date: currentDate,
+    });
+    response.json(meals);
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.put("/", validationFilter, async (request, response) => {
+  try {
+    const result = await knex("meals")
+      .where("id", "=", request.query.id)
+      .update(request.body);
+    if (result > 0) {
       response.send({ message: "Updated" });
     } else {
       response.status(404).send({ error: "id does not exist" });
     }
-    } catch (error) {
-        throw error; 
-    }     
+  } catch (error) {
+    throw error;
+  }
 });
 
-
-router.delete("/", validationFilter,async (request, response) => {
-    try {
-   
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-        const result = await knex("meals")
-            .where("id", "=", request.query.id)
-            .del();
-         if (result > 0) {
+router.delete("/", validationFilter, async (request, response) => {
+  try {
+    const result = await knex("meals").where("id", "=", request.query.id).del();
+    if (result > 0) {
       response.json({ message: "Deleted" });
     } else {
       response.status(404).json({ error: "id does not exist" });
     }
-    
   } catch (error) {
     throw error;
   }
 });
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/backend/api/reservations.js
+++ src/backend/api/reservations.js
@@ -1,91 +1,77 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("../database");
-const  inputValidation  = require("../validations/reservationsValidation")
-const validationFilter = require("../validations/filterValidation")
+const inputValidation = require("../validations/reservationsValidation");
+const validationFilter = require("../validations/filterValidation");
 
 const currentDate = new Date();
 
-router.get("/", validationFilter ,async (request, response) => {
+router.get("/", validationFilter, async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    if ('id' in request.query) {
+    if ("id" in request.query) {
       const reservations = await knex("reservations")
         .select()
-        .where('id', '=', request.query.id);
-      if (Object.keys((reservations)).length !== 0) {
+        .where("id", "=", request.query.id);
+      if (Object.keys(reservations).length !== 0) {
         response.json(reservations);
       } else {
-        response.status(404).json(`No reservation matches the id: ${request.query.id}`);
+        response
+          .status(404)
+          .json(`No reservation matches the id: ${request.query.id}`);
       }
     } else {
-      const reservations = await knex("reservations")
-        .select();
-       response.json(reservations);
+      const reservations = await knex("reservations").select();
+      response.json(reservations);
     }
-    
   } catch (error) {
     throw error;
   }
 });
 
+router.post("/", inputValidation, async (request, response) => {
+  try {
+    const insertRequest = request.body;
 
-
-router.post("/", inputValidation,async(request, response) => {
-    try {
-        // knex syntax for selecting things. Look up the documentation for knex for further info
-        const insertRequest = request.body;
-      
-        const reservations = await knex("reservations").insert({
-             
-            id: insertRequest.id,
-            number_of_guests: insertRequest.number_of_guests,
-            meal_id: insertRequest.meal_id,
-            created_date: currentDate,
-            contact_phonenumber: insertRequest.contact_phonenumber,
-            contact_name: insertRequest.contact_name,
-            contact_email: insertRequest.contact_email,
-        });
-        response.json(reservations);
-    } catch (error) {
-        throw error;
-    }
+    const reservations = await knex("reservations").insert({
+      id: insertRequest.id,
+      number_of_guests: insertRequest.number_of_guests,
+      meal_id: insertRequest.meal_id,
+      created_date: currentDate,
+      contact_phonenumber: insertRequest.contact_phonenumber,
+      contact_name: insertRequest.contact_name,
+      contact_email: insertRequest.contact_email,
+    });
+    response.json(reservations);
+  } catch (error) {
+    throw error;
+  }
 });
 
-
-router.put("/", validationFilter , async (request, response) => {
+router.put("/", validationFilter, async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-     
-      const result = await knex("reservations")
-          .where("id", "=", request.query.id)
-        .update(request.body);
-     if (result  > 0) {
+    const result = await knex("reservations")
+      .where("id", "=", request.query.id)
+      .update(request.body);
+    if (result > 0) {
       response.json({ message: "Updated" });
     } else {
       response.status(404).json({ error: "id does not exist" });
     }
-  
   } catch (error) {
     throw error;
   }
 });
 
-
-
-router.delete("/", validationFilter , async (request, response) => {
-    try {
-     
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-      const result = await knex("reservations")
-            .where("id", "=", request.query.id)
-        .del();
-       if (result  > 0) {
+router.delete("/", validationFilter, async (request, response) => {
+  try {
+    const result = await knex("reservations")
+      .where("id", "=", request.query.id)
+      .del();
+    if (result > 0) {
       response.json({ message: "Deleted" });
     } else {
       response.status(404).json({ error: "id does not exist" });
     }
-    
   } catch (error) {
     throw error;
   }
