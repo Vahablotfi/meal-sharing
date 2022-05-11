cefe531965e8632f8094fd65fcb3470580bd7bc8
Vahablotfi <vahablotfi1@gmail.com>
2022-05-11 22:57:45 +0200

    fixed- merge conflict

--- src/backend/validations/filterValidation.js
+++ src/backend/validations/filterValidation.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const Joi = require('joi')
-    .extend(require('@joi/date'));
-  
-
-    const filterValidSchema = Joi.object({
-    id: Joi.number().integer(),
-    title: Joi.string(),
-    maxPrice: Joi.number().precision(2),
-    limit: Joi.number().integer(),
-    createdAfter: Joi.date().format('YYYY-MM-DD'),
-    availableReservations: Joi.boolean(),
-});
-
-const validationFilter = (request, response, next) => {
-  const { error, value } = filterValidSchema.validate(request.query)
-=======
 const Joi = require("joi").extend(require("@joi/date"));
 
 const filterValidSchema = Joi.object({
@@ -28,7 +11,7 @@ const filterValidSchema = Joi.object({
 
 const validationFilter = (request, response, next) => {
   const { error, value } = filterValidSchema.validate(request.query);
->>>>>>> frontend
+
   if (error) {
     response.status(400).send(error.message);
   } else {
@@ -36,8 +19,4 @@ const validationFilter = (request, response, next) => {
   }
 };
 
-<<<<<<< HEAD
 module.exports = validationFilter;
-=======
-module.exports = validationFilter;
->>>>>>> frontend
--- src/backend/validations/reservationsValidation.js
+++ src/backend/validations/reservationsValidation.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-const Joi = require('joi')
-    .extend(require('@joi/date'));
-
-const currentDate = new Date();
-
-    const reservationValidSchema = Joi.object({
-    id: Joi.number().integer(),
-    number_of_guests:Joi.number().integer().required(),
-    meal_id: Joi.number().integer().required(), 
-    created_date: Joi.date().timestamp(),
-    contact_phonenumber:Joi.number().min(8).required(),
-    contact_name: Joi.string().min(4).required(),
-  contact_email:Joi.string().email(),
-    })
-
-
-    const inputValidation =  (request, response, next)=> {
-    const { error, value } = reservationValidSchema.validate(request.body)
-    if (error) {
-        response.status(400).send(error.message);
-    } else {
-        next();
-    }
-    }
-
-    module.exports = inputValidation;
-=======
 const Joi = require("joi").extend(require("@joi/date"));
 
 const currentDate = new Date();
@@ -35,7 +7,7 @@ const reservationValidSchema = Joi.object({
   number_of_guests: Joi.number().integer().required(),
   meal_id: Joi.number().integer().required(),
   created_date: Joi.date().timestamp(),
-  contact_phonenumber: Joi.number().required(),
+  contact_phonenumber: Joi.number().min(8).required(),
   contact_name: Joi.string().min(4).required(),
   contact_email: Joi.string().email(),
 });
@@ -50,4 +22,3 @@ const inputValidation = (request, response, next) => {
 };
 
 module.exports = inputValidation;
->>>>>>> frontend
